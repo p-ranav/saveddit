52da1a44d58d9562ffdad3c7b8b690af041e821c
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2021-04-08 09:33:58 -0500

    Added support for downloading user submissions

--- src/saveddit/user_downloader.py
+++ src/saveddit/user_downloader.py
@@ -10,7 +10,7 @@ from saveddit.submission_downloader import SubmissionDownloader
 from saveddit.configuration import ConfigurationLoader
 
 
-class SavedPostsDownloader():
+class UserDownloader():
     app_config_dir = os.path.expanduser("~/.saveddit")
     if not os.path.exists(app_config_dir):
         os.makedirs(app_config_dir)
@@ -43,7 +43,7 @@ class SavedPostsDownloader():
     IMGUR_CLIENT_ID = config['imgur_client_id']
     DEFAULT_POST_LIMIT = None
 
-    def __init__(self):
+    def __init__(self, username):
         self.logger = verboselogs.VerboseLogger(__name__)
         level_styles = {
             'critical': {'bold': True, 'color': 'red'},
@@ -60,18 +60,61 @@ class SavedPostsDownloader():
                             fmt='%(message)s', level_styles=level_styles)
 
         reddit = praw.Reddit(
-            client_id=SavedPostsDownloader.REDDIT_CLIENT_ID,
-            client_secret=SavedPostsDownloader.REDDIT_CLIENT_SECRET,
+            client_id=UserDownloader.REDDIT_CLIENT_ID,
+            client_secret=UserDownloader.REDDIT_CLIENT_SECRET,
             user_agent="saveddit (by /u/p_ranav)",
-            username=SavedPostsDownloader.REDDIT_USERNAME,
-            password=SavedPostsDownloader.REDDIT_PASSWORD
+            username=UserDownloader.REDDIT_USERNAME,
+            password=UserDownloader.REDDIT_PASSWORD
         )
-        self.user = reddit.redditor(name=SavedPostsDownloader.REDDIT_USERNAME)
+        self.username = username
+        self.user = reddit.redditor(name=username)
 
     def download(self, output_path, post_limit=DEFAULT_POST_LIMIT, skip_videos=False, skip_meta=False, skip_comments=False, comment_limit=0):
 
-        root_dir = os.path.join(os.path.join(os.path.join(os.path.join(
-            output_path, "www.reddit.com"), "user"), SavedPostsDownloader.REDDIT_USERNAME), "saved")
+        self.logger.notice("Downloading from /user/" + self.username)
+
+        root_dir = os.path.join(os.path.join(os.path.join(
+            output_path, "www.reddit.com"), "user"), self.username)
+
+        # Try and download submissions
+        try:
+            self.download_submissions(root_dir, post_limit, skip_videos, skip_meta, skip_comments, comment_limit)
+        except Exception as e:
+            self.logger.error("Unable to download submissions for user `" + self.username + "` - " + str(e))
+
+        # Try and download saved posts
+        try:
+            self.download_saved_posts(root_dir, post_limit, skip_videos, skip_meta, skip_comments, comment_limit)
+        except Exception as e:
+            self.logger.error("Unable to download saved posts/comments for user `" + self.username + "` - " + str(e))
+
+    def download_submissions(self, output_path, post_limit, skip_videos, skip_meta, skip_comments, comment_limit):
+        submissions_dir = os.path.join(output_path, "submissions")
+        if not os.path.exists(submissions_dir):
+            os.makedirs(submissions_dir)
+
+        self.logger.notice("Downloading from /user/" + self.username + "/submissions/")
+
+        categories = [ "hot", "new", "top", "controversial"]
+
+        for c in categories:
+            self.logger.verbose("Downloading submissions sorted by " + c)
+            category_function = getattr(self.user.submissions, c)
+
+            category_dir = os.path.join(submissions_dir, c)
+
+            if category_function:
+                for i, s in enumerate(category_function(limit=post_limit)):
+                    prefix_str = '#' + str(i) + ' '
+                    self.indent_1 = ' ' * len(prefix_str) + "* "
+                    self.indent_2 = ' ' * len(self.indent_1) + "- "
+                    SubmissionDownloader(s, i, self.logger, category_dir, skip_videos, skip_meta, skip_comments, comment_limit,
+                                            {'imgur_client_id': UserDownloader.IMGUR_CLIENT_ID})
+
+    def download_saved_posts(self, output_path, post_limit, skip_videos, skip_meta, skip_comments, comment_limit):
+        saved_dir = os.path.join(output_path, "saved")
+        if not os.path.exists(saved_dir):
+            os.makedirs(saved_dir)
 
         for i, s in enumerate(self.user.saved(limit=post_limit)):
             prefix_str = '#' + str(i) + ' '
@@ -82,15 +125,15 @@ class SavedPostsDownloader():
                     prefix_str + "Comment `" + str(s.id) + "` by " + str(s.author))
                 post_dir = str(i).zfill(4) + "_Comment_" + \
                     str(s.id) + "_by_" + str(s.author)
-                submission_dir = os.path.join(root_dir, post_dir)
+                submission_dir = os.path.join(saved_dir, post_dir)
                 self.download_saved_comment(s, submission_dir)
             elif isinstance(s, praw.models.Comment):
                 self.logger.verbose(
                     prefix_str + "Comment `" + str(s.id) + "` by " + str(s.author))
                 self.logger.spam(self.indent_2 + "Skipping comment")
             elif isinstance(s, praw.models.Submission):
-                SubmissionDownloader(s, i, self.logger, root_dir, skip_videos, skip_meta, skip_comments, comment_limit,
-                                     {'imgur_client_id': SavedPostsDownloader.IMGUR_CLIENT_ID})
+                SubmissionDownloader(s, i, self.logger, saved_dir, skip_videos, skip_meta, skip_comments, comment_limit,
+                                     {'imgur_client_id': UserDownloader.IMGUR_CLIENT_ID})
             else:
                 pass
 
